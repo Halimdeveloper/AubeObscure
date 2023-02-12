7ee4880c18ddbdb89f27e00870d51ebfb88bc92d
Matthieu <thierry.matthieu@live.fr>
2023-02-12 23:49:13 +0100

    Les Radio Button ça marche

--- frontend/src/Components/GameMaster/index.tsx
+++ frontend/src/Components/GameMaster/index.tsx
@@ -1,6 +1,6 @@
 import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from "@mui/material";
 import React from "react";
-import RadioBtn2 from "../RadioBtn";
+import RadioBtn from "../RadioBtn";
 
 
 export default function gameMaster() {
@@ -43,7 +43,7 @@ export default function gameMaster() {
         <h1>Bienvenue Maître </h1>
         <div>Voici les listes des joueurs du jour :<div>{toto(players)}</div></div> { /* stat + classe/atout/malus */}
         {/* <div>{RadioBtn("Gandalf", "Prout", "caca")}</div> */}
-        <RadioBtn2  x="1" y="test2" z="test3" onChange={handleChange} />
+        <RadioBtn  x="1" y="test2" z="test3" onChange={handleChange} />
         <p>Voici ce que les joueurs mort ont ramener au village :</p>
         <p>Voici ou vous en êtes dans l'histoire :</p>
         <p>Lancer la partie</p>
--- frontend/src/Components/RadioBtn/index.tsx
+++ frontend/src/Components/RadioBtn/index.tsx
@@ -1,5 +1,5 @@
 import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from "@mui/material";
-import React from 'react';
+import React from "react"
 
 type Props = {
   x: string;
@@ -8,16 +8,11 @@ type Props = {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const RadioBtn2: React.FC<Props> = ({ x, y, z, onChange }) => {
+const RadioBtn: React.FC<Props> = ({ x, y, z, onChange }) => {
   return (
     <FormControl component="fieldset">
-      <FormLabel component="legend">Liste des joueurs :</FormLabel>
-      <RadioGroup 
-        onChange={onChange} 
-        aria-labelledby="demo-radio-buttons-group-label"
-        defaultValue={x}
-        name="radio-buttons-group"
-      >
+      <FormLabel component="legend">Les voici</FormLabel>
+      <RadioGroup onChange={onChange}>
         <FormControlLabel value={x} control={<Radio />} label={x} />
         <FormControlLabel value={y} control={<Radio />} label={y} />
         <FormControlLabel value={z} control={<Radio />} label={z} />
@@ -26,5 +21,4 @@ const RadioBtn2: React.FC<Props> = ({ x, y, z, onChange }) => {
   );
 };
 
-export default RadioBtn2;
-  
+export default RadioBtn;
