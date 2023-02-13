a0b3eaf25c9ea1d4a8954ab1d481fd97ee92ec07
Matthieu <thierry.matthieu@live.fr>
2023-02-13 01:33:39 +0100

    Test Boutons players + ennemy

--- frontend/src/Components/GameMaster/index.tsx
+++ frontend/src/Components/GameMaster/index.tsx
@@ -1,10 +1,17 @@
 import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from "@mui/material";
 import React from "react";
-import RadioBtn from "../RadioBtn";
-
+import RadioBtn from "../RadioBtn"
+import ButtonRow from "./ButtonRow";
 
 export default function gameMaster() {
 
+    const handleLeftClick = () => {
+        // votre logique pour le bouton gauche
+    };
+
+    const handleRightClick = () => {
+        // votre logique pour le bouton droit
+    };
 
     type Player = {
         firstName: string
@@ -34,7 +41,7 @@ export default function gameMaster() {
         })
     }
 
-    function handleChange(event:any) {
+    function handleChange(event: any) {
         console.log(event.target.value);
     }
 
@@ -43,10 +50,17 @@ export default function gameMaster() {
         <h1>Bienvenue Maître </h1>
         <div>Voici les listes des joueurs du jour :<div>{toto(players)}</div></div> { /* stat + classe/atout/malus */}
         {/* <div>{RadioBtn("Gandalf", "Prout", "caca")}</div> */}
-        <RadioBtn  x="1" y="test2" z="test3" onChange={handleChange} />
+        <RadioBtn x="1" y="test2" z="test3" onChange={handleChange} />
         <p>Voici ce que les joueurs mort ont ramener au village :</p>
         <p>Voici ou vous en êtes dans l'histoire :</p>
         <p>Lancer la partie</p>
-    </div>;
+        <div>
+            <ButtonRow
+                leftButtonText={['Bouton gauche 1', 'Bouton gauche 2', 'Bouton gauche 3', 'Bouton gauche 4']}
+                rightButtonText={['Bouton droit 1', 'Bouton droit 2', 'Bouton droit 3', 'Bouton droit 4']}
+            />
+        </div>
+    </div>
+
 }
 
