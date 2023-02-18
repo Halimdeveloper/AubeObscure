d2d051cc4618d14288e85ebd4b72bdd4e0b1bbb6
Matthieu <thierry.matthieu@live.fr>
2023-02-18 16:28:19 +0100

    ajout du reset css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import App from './App'
+import './index.css'
+import './reset.css'
 import { BrowserRouter } from 'react-router-dom'
+import { StyledEngineProvider } from '@mui/material/styles'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-root.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <StyledEngineProvider injectFirst>
+        <App />
+      </StyledEngineProvider>
     </BrowserRouter>
   </React.StrictMode>
-);
+)
