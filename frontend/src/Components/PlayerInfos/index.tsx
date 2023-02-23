b5ed89c649a8e978db661db572b4ce0c5aa0b3d6
Matthieu <thierry.matthieu@live.fr>
2023-02-23 17:39:45 +0100

    Ajout visuel de 2 boutons en bas de playerinfo et un peu d'habillage

--- frontend/src/Components/PlayerInfos/index.tsx
+++ frontend/src/Components/PlayerInfos/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material"
 import "./style.css"
 
 
@@ -7,8 +8,14 @@ export default function PlayerInfos() {
 
   return (
     <div className="playerInfos">
+      <div>
       <h1>Name</h1>
       <h2>Stats</h2>
+      </div>
+      <div className="playerButton">
+      <Button variant="outlined" color="success" style={{ backgroundColor: "black" }} >Stats</Button>
+      <Button variant="outlined" color="success" style={{ backgroundColor: "black" }}>Inventaire</Button>
+      </div>
     </div>
 
   )
