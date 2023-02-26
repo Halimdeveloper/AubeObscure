c57f0c1bc5a31b1e16a9267aecd0ebfca9b85703
pierre.goemans <pierre.goemans.ext@melting-point.fr>
2023-02-26 20:54:35 +0100

    devtool for store

--- frontend/src/stores/DiceStore.ts
+++ frontend/src/stores/DiceStore.ts
@@ -1,7 +1,17 @@
 import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
 
 // Store pour les données de "dés"
-export const useDiceStore = create((set) => ({
-  dices: [],
-  setDices: (dices:any) => set({ dices })
-}));
+export const useDiceStore = create()(
+  devtools(
+    persist(
+      (set) => ({
+        dices: [],
+        setDices: (dices: any) => set({ dices }),
+      }),
+      {
+        name: "Dice-storage",
+      }
+    )
+  )
+);
