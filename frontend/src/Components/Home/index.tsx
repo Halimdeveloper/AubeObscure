623efd88f2c424c8f6aa8d65965c82fe22443473
Matthieu <thierry.matthieu@live.fr>
2023-02-27 12:17:09 +0100

    Modif Home page

--- frontend/src/Components/Home/index.tsx
+++ frontend/src/Components/Home/index.tsx
@@ -16,47 +16,83 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <p>Quel est ton nom ?</p>
-      {/* <input type="text" name="name" value={namePlayer} onChange={e => setNamePlayer(e.target.value)} /> */}
+    <div className="bodyHome">
+      <strong className="titreGlobal">Aube Obscure</strong>
+      <div className="pHomeCss">
+        <div>
+          Vous êtes sur le point d'entrer dans un monde de ténèbres et de
+          mystères, où la magie règne en maître et où chaque choix peut mener à
+          la gloire ou à la mort. Aube Obscure est un jeu de rôle qui vous
+          plongera dans un univers fantastique rempli de créatures étranges et
+          de lieux mystérieux.
+        </div>
+        <div>
+          En tant qu'aventurier, vous explorerez des donjons sombres et
+          dangereux, combattrez des ennemis redoutables, résoudrez des énigmes
+          complexes et découvrirez des trésors fabuleux. Vous devrez faire
+          preuve de courage, de sagesse et d'habileté pour survivre dans ce
+          monde impitoyable.
+        </div>
+        <div>
+          Votre personnage est déterminé aléatoirement, adapté votre stratégie
+          en fonction de votre ligné et de vos dons. Seul la persistence de vos
+          connaissance et de vos objets sera transmis à vos descendants.
+        </div>
+        <div>
+          Aube Obscure vous propose une expérience de jeu immersive, où la
+          liberté de choix et l'imagination sont les seules limites. Alors,
+          êtes-vous prêt à affronter les ténèbres ? Le sort du monde repose
+          entre vos mains."
+        </div>
+      </div>
+      <div className="formCss">
+        <h2 className="titreSecond">Quel est ton nom ?</h2>
+        {/* <input type="text" name="name" value={namePlayer} onChange={e => setNamePlayer(e.target.value)} /> */}
+        <div className="formContainer">
+          <FormControl fullWidth style={{ color: "rgba(7, 0, 124, 1)" }}>
+            <InputLabel id="demo-simple-select-label">Votre nom :</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={userName}
+              label="om"
+              onChange={setUserLocally}
+            >
+              <MenuItem value={"Halim"}>Halim</MenuItem>
+              <MenuItem value={"Pierre"}>Pierre</MenuItem>
+              <MenuItem value={"Matthieu"}>Matthieu</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
 
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Votre nom :</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={userName}
-          label="om"
-          onChange={setUserLocally}
-        >
-          <MenuItem value={"Halim"}>Halim</MenuItem>
-          <MenuItem value={"Pierre"}>Pierre</MenuItem>
-          <MenuItem value={"Matthieu"}>Matthieu</MenuItem>
-        </Select>
-      </FormControl>
-
-      <Button
-        variant="contained"
-        onClick={() =>
-          setCurrentUser({
-            name: userName,
-            role: RoleEnum.Player,
-          })
-        }
-      >
-        Player
-      </Button>
-      <Button
-        variant="contained"
-        onClick={() =>
-          setCurrentUser({
-            name: userName,
-            role: RoleEnum.GM
-          })
-        }
-      >
-        GM
-      </Button>
+        <div className="buttonContainer">
+          <Button
+            style={{ fontSize: "1.5rem" }}
+            variant="contained"
+            onClick={() =>
+              setCurrentUser({
+                name: userName,
+                role: RoleEnum.Player,
+              })
+            }
+          >
+            Player
+          </Button>
+          <Button
+            style={{ fontSize: "1.5rem" }}
+            variant="contained"
+            onClick={() =>
+              setCurrentUser({
+                name: userName,
+                role: RoleEnum.GM,
+              })
+            }
+          >
+            GM
+          </Button>
+        </div>
+      </div>
+      <div></div>
     </div>
   );
 }
